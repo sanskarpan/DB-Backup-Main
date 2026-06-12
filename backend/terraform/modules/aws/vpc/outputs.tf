f64a5cf3cbba582fcd51948e1add642a6e2b6e4b
sanskarpan <sanskarpandey2004@gmail.com>
2026-06-12 13:37:31 +0530

    add Terraform AWS VPC module outputs for subnet and route table IDs refs #83

---
+++ backend/terraform/modules/aws/vpc/outputs.tf
@@ -0,0 +1,51 @@
+# AWS VPC Module Outputs
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  description = "IDs of database subnets"
+  value       = var.enable_database_subnets ? aws_subnet.database[*].id : []
+}
+
+output "database_subnet_group_name" {
+  description = "Name of database subnet group"
+  value       = var.enable_database_subnets ? aws_db_subnet_group.main[0].name : null
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of NAT Gateways"
+  value       = aws_nat_gateway.main[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "public_route_table_id" {
+  description = "ID of public route table"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "IDs of private route tables"
+  value       = aws_route_table.private[*].id
+}
