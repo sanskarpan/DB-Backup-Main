d5b6bf6e664ea2732457643105ab20733783c69e
sanskarpan <sanskarpandey2004@gmail.com>
2026-06-17 11:16:56 +0530

    feat: update desktop app entry point with improved initialization

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import EnhancedApp from './EnhancedApp';
+import { ErrorBoundary } from './components/ErrorBoundary';
 import './index.css';
 import './i18n/config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <EnhancedApp />
+    <ErrorBoundary>
+      <EnhancedApp />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
